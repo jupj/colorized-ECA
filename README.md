cefdf589e5754fec3b17d83609e957fb86175f83
Johan Jansson <johan.jansson@iki.fi>
2023-09-10 19:27:04 +0300

    Generate pngs with bw and color diagrams of ECA

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This means that there are 2^3 = 8 possible state configurations that determine t
 
 By assigning a color to numbers 0-7, we can color-code each cell.
 For example, if the left neighbor is 'on' (1), the cell itself 'off' (0) and the right neighbor 'off' (0); this state configuration is encoded as binary 100, which translates to color number 4 in decimal.
+
+To generate these diagrams, simply run `go run main.go`
--- main.go
+++ main.go
@@ -1,20 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"os"
+)
 
 func main() {
+	const n = 64 // number of iterations
+
+	// Generate color and bw pngs for all elementary cellular automata
 	for i := 0; i < 256; i++ {
 		fmt.Printf("*** Rule %d ***\n", i)
-		printECA(i)
-		fmt.Println()
+		//printECA(i, n)
+		if err := plotECA(fmt.Sprintf("rule%.3d_color.png", i), i, n, true); err != nil {
+			log.Fatal(err)
+		}
+		if err := plotECA(fmt.Sprintf("rule%.3d_bw.png", i), i, n, false); err != nil {
+			log.Fatal(err)
+		}
 	}
+
+	fmt.Println("done")
+}
+
+// plotECA plots the space-time diagram for an ECA, starting with a single
+// 'on' input
+// The result is stored as a png file
+func plotECA(filename string, rule, n int, useColor bool) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// The output width will be n. Add n cells on each side to avoid edge
+	// effects from affecting the diagram
+	width := 3 * n
+
+	grid := make([]cell, width)
+	grid[width/2].on = true
+
+	pal := color.Palette([]color.Color{
+		color.RGBA{85, 85, 85, 255},   // Gray
+		color.RGBA{255, 85, 85, 255},  // red
+		color.RGBA{85, 255, 85, 255},  // green
+		color.RGBA{255, 255, 85, 255}, // yellow
+		color.RGBA{85, 85, 255, 255},  // blue
+		color.RGBA{255, 85, 255, 255}, // magenta
+		color.RGBA{85, 255, 255, 255}, // cyan
+		color.Black,
+		color.White,
+	})
+
+	size := 10 // cell size in pixels
+	img := image.NewPaletted(image.Rect(0, 0, n*size, n*size), pal)
+	b := img.Bounds()
+
+	xoff := (len(grid) - n) / 2 // offset to for n cells in the center
+	for y := 0; y < n; y++ {
+		for x := 0; x < n; x++ {
+			c := grid[x+xoff]
+			for dx := 0; dx < size; dx++ {
+				for dy := 0; dy < size; dy++ {
+					if c.on {
+						if useColor {
+							img.SetColorIndex(x*size+dx, y*size+dy, c.prevgen)
+						} else {
+							img.Set(x*size+dx, y*size+dy, color.Black)
+						}
+					} else {
+						img.Set(x*size+dx, y*size+dy, color.White)
+					}
+				}
+			}
+		}
+		grid = nextGen(grid, rule)
+	}
+
+	if err := png.Encode(f, img.SubImage(b)); err != nil {
+		return err
+	}
+	return nil
 }
 
 // printECA prints the space-time diagram for an ECA, starting with a single
 // 'on' input
-func printECA(rule int) {
-	const n = 20 // number of iterations
-
+func printECA(rule, n int) {
 	// The output width will be 2*n - 1. Add n cells on each side to avoid edge
 	// effects from affecting the diagram
 	width := 2*n - 1 + 2*n
@@ -56,6 +131,7 @@ func nextGen(grid []cell, rule int) []cell {
 	return next
 }
 
+// printGrid prints an ECA grid as a line to stdout
 func printGrid(grid []cell) {
 	for _, c := range grid {
 		if c.on {
